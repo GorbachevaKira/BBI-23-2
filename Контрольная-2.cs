0a4356fd5bd2ab60a06c4e1843f22eed37fffe32
GorbachevaKira <159521683+GorbachevaKira@users.noreply.github.com>
2024-04-26 11:55:58 +0300

    Add files via upload

---
+++ Контрольная-2.cs
@@ -0,0 +1,10 @@
+﻿//3
+//string path = @"C:\Users\m2303906\Documents";
+//string folderName = "Test";
+//path = Path.Combine(path, folderName);
+//if (!Directory.Exists(path))
+//{ 
+    //Directory.CreateDirectory(path);
+//}
+//string n1 = "task_1.json";
+//string n2 = "task_1.json";
